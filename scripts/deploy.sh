c66d08ab01017bc1dc5149934029adb329541bb5
David Thigpen <davidthigs@gmail.com>
2025-04-11 14:26:38 -0600

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # This is the site output dir
-BUILD_DIR="${SCRIPT_DIR}/../build"
+BUILD_DIR="build"
 # Temp commit message that will be removed after
 TMP_COMMIT_MSG="Temporary commit for subtree"
 
@@ -24,12 +24,22 @@ git add -f build
 git commit -m "${TMP_COMMIT_MSG}"
 
 # Push using subtree
-git subtree push --prefix build origin gh-pages; ret=$?
+if git subtree split --prefix "${BUILD_DIR}" -b gh-pages-temp
+then
+	ret=$?
+	# Force push it to the gh-pages branch on origin
+	git push -f origin gh-pages-temp:gh-pages
+	
+	# Clean up local temp branch
+	git branch -D gh-pages-temp
+else
+	ret=$?
+fi
 last_msg="$(git log -1 --pretty=format:%B)"
 
 if [[ "${last_msg}" == "${TMP_COMMIT_MSG}" ]]
 then
-	exho 'Undoing last temporary commit'
+	echo 'Undoing last temporary commit'
 	git reset HEAD~
 else
     echo "Something failed. Last commit was not temporary deploy commit"
