f58cae1520f192016c1ddef8473d672a6422c450
David Thigpen <davidthigs@gmail.com>
2025-04-11 14:09:45 -0600

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# This is the site output dir
+BUILD_DIR="${SCRIPT_DIR}/../build"
+# Temp commit message that will be removed after
+TMP_COMMIT_MSG="Temporary commit for subtree"
+
+# Build the site
+# "${SCRIPT_DIR}/bic" .
+
+if [[ ! -d "${BUILD_DIR}" ]]
+then
+	echo "Output dir does not exist: ${BUILD_DIR}"
+	exit 1
+fi
+
+# Force-add the ignored folder
+git add -f build
+
+# Commit if needed (you can do this on a throwaway branch or stash it afterward)
+git commit -m "${TMP_COMMIT_MSG}"
+
+# Push using subtree
+git subtree push --prefix build origin gh-pages; ret=$?
+last_msg="$(git log -1 --pretty=format:%B)"
+
+if [[ "${last_msg}" == "${TMP_COMMIT_MSG}" ]]
+then
+	exho 'Undoing last temporary commit'
+	git reset HEAD~
+else
+    echo "Something failed. Last commit was not temporary deploy commit"
+fi
+
+exit $ret
+
+
